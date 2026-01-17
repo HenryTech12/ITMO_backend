c1959db970ae7a2516bd80b51395cab352b9c9e6
HenryTech12 <fakorodehenry@gmail.com>
2026-01-17 21:51:04 +0100

    implement user authentication

--- service/userService.py
+++ service/userService.py
@@ -12,4 +12,13 @@ def create_user(data,db):
         user = userModel.UserModel(name=data.name,email=data.email,password_hash=data.password, phone_number=data.phone_number)
         return user
     
-    
+def login(data,db):
+    if data.email:
+        user_db = db.query(userModel.UserModel).filter(userModel.UserModel.email == data.email).first()
+        if user_db:
+            if securityConfig.verify_hash(data.password, user_db.password):
+                return securityConfig.create_token(data)
+            else:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid User Details')
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User account not found')
