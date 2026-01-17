d29cc71255a937f53cdd86e28e677a93595b5bdf
HenryTech12 <fakorodehenry@gmail.com>
2026-01-17 21:58:06 +0100

    implemented desk service

---
+++ service/deskService.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException, status
+from models import deskModel
+import requests
+
+
+def get_desks(data,db):
+    try:
+        response = requests.get('')
+        if response.status_code == 200:
+            return response
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='An error occurred, when calling api')
+    
+    
